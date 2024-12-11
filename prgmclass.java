96d6dd85743a627528d802fa2582b56026f64aa1
Rishitha <trishithapandu2218@gmail.com>
2024-12-11 11:02:55 +0530

    Add files via upload

---
+++ prgmclass.java
@@ -0,0 +1,11 @@
+class a1{
+    int a=50;
+    int b=20;
+}
+class prgmclass{
+    public static void main(String args[]){
+        a1 r=new a1();
+        System.out.println(r.a);
+        System.out.println(r.b);
+    }
+}
