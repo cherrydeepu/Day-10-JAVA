2281ad0c5832e86d9cf4eb01271123b4d6c37d0a
Rishitha <trishithapandu2218@gmail.com>
2024-12-11 12:35:46 +0530

    Add files via upload

---
+++ staticwithnonvoid1.java
@@ -0,0 +1,11 @@
+class staticwithnonvoid1{
+    static String s()
+    {
+        return "Hello Rishitha";
+    }
+    public static void main(String[] arg)
+    {
+        String res = s();
+        System.out.println(res);
+    }
+}
